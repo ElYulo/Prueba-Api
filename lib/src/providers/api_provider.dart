6f8230ffa861d1e8957175504e65e76e6e4a204f
Saul Tellez <saul.t.n.m@gmail.com>
2021-07-02 14:49:17 -0700

    Model and Provider about API Rick and Morti by Saul Tellez(ElYulo)

---
+++ lib/src/providers/api_provider.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:apiprueba/src/models/api_models.dart';
+
+class ApiProvider {
+  final _url = 'https://rickandmortyapi.com/api/character';
+  final _http = Dio();
+
+//obtener personajes ya contiene los datos de la api para hacer el diseño de la app
+
+  Future<List<ApiModel>> obtenerPersonajes() async {
+    final response = await _http.get(_url);
+    List<dynamic> data = response.data;
+    return data.map((api) => ApiModel.transInstancia(api)).toList();
+  }
+}
